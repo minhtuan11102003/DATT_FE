2a9ff4eb48ad3bfd7cfd521b79fcdd1f4872757e
MikeDao <maidao170903@gmail.com>
2024-10-17 22:05:51 +0700

    done page Gallery

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Nunito, Roboto } from 'next/font/google'
-import HeaderComponent from "../components/page/header";
-import FooterComponent from "../components/page/footer";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Nunito, Roboto } from 'next/font/google';
+import HeaderComponent from '../components/page/header';
+import FooterComponent from '../components/page/footer';
 
 const nunito = Nunito({
   subsets: ['latin'],
   display: 'swap',
-})
-
+});
 
 export const metadata: Metadata = {
-  title: "Dự án thực tập",
-  description: "Admin app",
+  title: 'Dự án thực tập',
+  description: 'Admin app',
 };
 
 export default function RootLayout({
@@ -22,11 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={`${nunito.className} text-primary !scroll-smooth`}>
+      <body
+        suppressHydrationWarning={true}
+        className={`${nunito.className} text-primary !scroll-smooth`}
+      >
         <div className="w-full">
-          <HeaderComponent/>
-            {children}
-          <FooterComponent/>
+          <HeaderComponent />
+          {children}
+          <FooterComponent />
         </div>
       </body>
     </html>
