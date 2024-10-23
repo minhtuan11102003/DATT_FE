2dc0170362a2112c47a042957702c03ab2428d84
Khangvt384 <khangvt384@gmail.com>
2024-10-23 16:23:50 +0700

    detail success

--- src/app/Accomodation/page.tsx
+++ src/app/Accomodation/page.tsx
@@ -727,7 +727,6 @@ const AccomodationPage = () => {
                     <div className="overflow-hidden pb-[30px] sm:text-center">
 
                       <figure className="figure-hover relative sm:text-center sm:inline-block ">
-
                         <div className="mb-[28px]">
                           <a>
                             <img
@@ -800,7 +799,6 @@ const AccomodationPage = () => {
                     <div className="overflow-hidden pb-[30px] sm:text-center">
 
                       <figure className="figure-hover relative sm:text-center sm:inline-block ">
-
                         <div className="mb-[28px]">
                           <a>
                             <img
@@ -833,7 +831,7 @@ const AccomodationPage = () => {
                           </div>
                         </div>
 
-                        <div className="bounch-hover p-[13px] bg-[#f7c411] hidden bg-repeat h-full w-full text-left top-0 left-0 bottom-0 right-0 animate-bounceInDown animate-mode absolute duration-700 sm:text-center md:text-left ">
+                        <div className="bounch-hover p-[13px] bg-[#f7c411] hidden bg-repeat h-full w-full text-left top-0 left-0 bottom-0 right-0 animate-bounceInDown animate-mode absolute duration-700 sm:text-center md:text-left">
                           <div className="border-[1px] border-[#475362] md:p-[13px] p-[22px] lg:p-[22px] lg:h-full">
                             <h5 className="text-[18px] md:text-[12px] lg:text-[16px] xl:text-[18px] text-[#000] font-Lora leading-5 font-normal">
                               Deluxe Room
@@ -857,20 +855,19 @@ const AccomodationPage = () => {
                                   </span>
                                 </p>
                               </div>
-
-                              <a className="block mb-[-23px] mr-[-23px] md:mr-[-13px] md:mb-[-5px]  xl:mr-[-23px] cursor-pointer border-t-[1px] border-l-[1px] text-[#444] md:text-[10px] py-[8px] px-[17px] font-normal font-Lora lg:text-[14px] md:py-[7px] md:px-[14px] xl:text-[14px] xl:py-[8px] xl:px-[17px]  hover:bg-[#444] hover:text-[#fff] hover:border-none lg:py-[8px] lg:px-[17px] lg:mr-[-23px] lg:mb-[-13px] xl:mb-[-18px] leading-[20px]">
-
-                                Book
-                              </a>
+                              <div className="right flex items-end ">
+                                <a className="block mb-[-23px] mr-[-23px] md:mr-[-13px] md:mb-[-5px]  xl:mr-[-23px] cursor-pointer border-t-[1px] border-l-[1px] text-[#444] md:text-[10px] py-[8px] px-[17px] font-normal font-Lora lg:text-[14px] md:py-[7px] md:px-[14px] xl:text-[14px] xl:py-[8px] xl:px-[17px]  hover:bg-[#444] hover:text-[#fff] hover:border-none lg:py-[8px] lg:px-[17px] lg:mr-[-23px] lg:mb-[-13px] xl:mb-[-18px] leading-[20px]">
+                                  Book
+                                </a>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </figure>
                     </div>
                   </div>
+
                 </div>
-
-
               </div>
             </div>
 
@@ -939,6 +936,71 @@ const AccomodationPage = () => {
                 </ul>
               </nav>
             </div>
+          </div>
+          <div className="other_decent_room_pagination">
+            <nav className="text-center mt-[65px] mb-[75px] w-[95%] mx-auto">
+              <ul className="pagination flex justify-center items-center">
+                <li>
+                  <a
+                    className="pagination__link text-[12px] font-bold p-[10px] md:text-[16px] uppercase"
+                    href=""
+                  >
+                    <IoIosArrowBack className="my-[3px] mx-[9px] inline-block font-bold" />
+                    prev
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="pagination__link  pagiantion__link--active text-[10px] p-[10px] md:text-[16px] uppercase"
+                    href=""
+                  >
+                    1
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="pagination__link text-[12px] font-bold p-[10px] md:text-[16px] uppercase"
+                    href=""
+                  >
+                    2
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="pagination__link text-[12px] font-bold p-[10px] md:text-[16px] uppercase"
+                    href=""
+                  >
+                    3
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="pagination__link text-[12px] font-bold p-[10px] md:text-[16px] uppercase"
+                    href=""
+                  >
+                    4
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="pagination__link text-[12px] font-bold p-[10px] md:text-[16px] uppercase"
+                    href=""
+                  >
+                    5
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="pagination__link text-[12px] p-[10px] font-bold md:text-[16px] uppercase"
+                    href=""
+                  >
+                    {" "}
+                    next{" "}
+                    <IoIosArrowForward className="my-[3px] mx-[9px] inline-block" />
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </section >
