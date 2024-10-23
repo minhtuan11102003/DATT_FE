6e5ca80a4507f629606b4cfb62cbb13f1d317c1a
Khangvt384 <khangvt384@gmail.com>
2024-10-23 16:23:50 +0700

    detail success

--- src/app/Detail/components/slider_detail_room.tsx
+++ src/app/Detail/components/slider_detail_room.tsx
@@ -27,15 +27,13 @@ const Slider_detail_room = ({sliders}: ImageSliderprops) => {
     return (
         <div className="slide-container">
             <div className="slider-show relative">
-
-                <div className="slides overflow-hidden w-[270px]">
-                    <ul className="w-[2700px] h-[471px] flex relative list-none">
-
+                <div className="slides overflow-hidden">
+                    <ul className="w-[2700px] sm:w-[4200px] md:w-[7200px] lg:w-[6980px] xl:w-[8480px] flex relative list-none">
                         {sliders.map((slider, index) => {
                             return (
-                                <li key={index}>
+                                <li key={index} className='sm:w-[420px] w-[270px] md:w-[720px] lg:w-[698px] xl:w-[848px]'>
                                     <img
-                                        className="w-full max-w-full h-auto object-cover duration-300 ease-in delay-300"
+                                        className="w-full object-center max-w-full h-auto object-cover  duration-500 ease delay-300"
                                         src={`/images/Detail/Deluxe_Room/${slider.image}`}
                                         alt={slider.alt}
                                         style={{ translate: `${-100 * imageIndex}%` }}
@@ -46,23 +44,23 @@ const Slider_detail_room = ({sliders}: ImageSliderprops) => {
                        
                     </ul>
                 </div>
-                <div className="btn-active w-full absolute top-[50%] translate-y-[-50%] flex justify-between ">
-                    <span className="btn-next pl-5 text-[32px] opacity-50 text-[#fff] cusor-pointer">
+                <div className="btn-active w-full absolute top-[50%] translate-y-[-50%] flex justify-between px-2">
+                    <span className="btn-next text-[32px] opacity-50 text-[#fff] cursor-pointer">
                         <IoIosArrowDropleft onClick={showPrevImage}/>
                     </span>
-                    <span className="btn-prev pr-5 text-[32px] opacity-50 text-[#fff]cusor-pointer">
+                    <span className="btn-prev text-[32px] opacity-50 text-[#fff] cursor-pointer">
                         <IoIosArrowDropright onClick={showNextImage}/>
                     </span>
                 </div>
             </div>
-            <div className="slider-thumnail mt-[20px] overflow-hidden w-[848px]">
-                <ul className="flex gap-6 w-[1163.17px]">
+            <div className="slider-thumnail mt-[20px] w-full overflow-hidden">
+                <ul className="flex gap-6 w-[392.6px] sm:w-[592.5px] md:w-[992.5px] lg:w-[963.167px] xl:w-[1163.17px]">
                     {
                         sliders.map((slider, index)=>{
                             return (
 
                                 <li key={index}
-                                    className="w-full h-full object-cover hover:cursor-pointer hover:opacity-50 active"
+                                    className="w-[25px] sm:w-[50px] md:w-[100px] lg:w-[96.33333333333333px] xl:w-[121.33333333333333px] hover:cursor-pointer hover:opacity-50 active"
                                 // onclick="switchImage(this)"
                                 >
                                     <img
