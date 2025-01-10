087893da2a4e111a4ebb0281d1d3cbf7a522a784
minhtuan11102003 <nx.minhtuan@gmail.com>
2025-01-10 09:00:39 +0700

    fix get api

--- src/app/_component_section/showProduct.tsx
+++ src/app/_component_section/showProduct.tsx
@@ -1,36 +1,33 @@
-'use client'
+'use client';
 import React, { useEffect, useState } from 'react';
 import CardProducts from './card_products';
+import axios from 'axios';
 
 const ListProduct = () => {
     const [products, setProducts] = useState<any>([]);
+
     useEffect(() => {
         const getProductsApi = async () => {
             try {
-                const payProdcuts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/product`);
-                if (!payProdcuts.ok) {
-                    throw new Error('Lỗi rồi kìa');
-                } else {
-                    const resProduct = await payProdcuts.json();
-                    setProducts([...resProduct.data.data]);
+                const payProdcuts = await axios.get(`http://api-core.dsp.one/api/client/product`);
+                console.log(payProdcuts.data);
+                if (payProdcuts.status === 200) {
+                    const data = payProdcuts.data?.data.data;
+                    setProducts([...data]);
                 }
-
             } catch (error) {
-                console.log(error);
+                console.error("Lỗi khi gọi API:", error);
             }
-        }
+        };
         getProductsApi();
-    }, [])
-
+    }, []);
 
     return (
         <div className="accomodation_room listProduct">
             <div className="md:grid md:grid-cols-4 gap-8">
-                {
-                    products.map((pr: any, index: number) => (
-                        <CardProducts data={pr} key={index} />
-                    ))
-                }
+                {products.map((pr: any, index: number) => (
+                    <CardProducts data={pr} key={index} />
+                ))}
             </div>
         </div>
     );
